f37b046ecb33b41114c73b5b8d15a2fe0586f670
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-18 10:14:38 -0500

    implemented 'clear' button functionality

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.on-screen-text');
+const clearButton = document.querySelector('button.clear');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        display.textContent = button.value;
+        display.textContent += button.value;
     });
+    clear();
 })
 
+const operator = {
+    '+': add(),
+    '-': subtract(),
+    '*': multiply(),
+    '/': divide(),
+}
 
+function clear() {
+    clearButton.addEventListener('click', () => {
+        display.textContent = " ";
+    });
+}
 
-const operators = ['+','-','*','/'];
+function operate(a, b, op) {
 
-
-
+}
 
 function add(a, b) {
     return a + b;
